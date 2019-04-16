f6a93445305985d3812d08c1c06da3ec5b15560c
Daniels <daniel.sadowski75@gmail.com>
2019-04-16 23:31:46 +0200

    added details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipies: Recipe[]= [
+    new Recipe('Recipe1', 'Desc1',
+      'https://www.wholesomeyum.com/wp-content/uploads/2016/08/wholesomeyum_taco-salad-2.jpg'),
+    new Recipe('Recipe2', 'Desc2',
+      'https://img.bestrecipes.com.au/B05gGuy3/w643-h428-cfill-q90/br/2017/05/super-easy-pizza-dough-recipe-520771-1.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
